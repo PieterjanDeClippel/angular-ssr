eef89b8557852a6a8de6d4eddee03fce2ff823d8
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-03-16 14:02:25 +0100

    Add angular app

--- angular-ssr/Program.cs
+++ angular-ssr/Program.cs
@@ -1,3 +1,6 @@
+using MintPlayer.AspNetCore.SpaServices.Extensions;
+using System.Text.RegularExpressions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddHealthChecks();
+
+builder.Services.AddSpaStaticFilesImproved(configuration =>
+{
+    configuration.RootPath = "ClientApp/dist/client-app/browser";
+});
 
 var app = builder.Build();
 
@@ -20,11 +28,22 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.UseHealthChecks("/healthz");
-app.MapGet("/", (context) =>
-{
-    context.Response.Redirect("/WeatherForecast");
-    return Task.CompletedTask;
-});
+//app.MapGet("/", (context) =>
+//{
+//    context.Response.Redirect("/WeatherForecast");
+//    return Task.CompletedTask;
+//});
 app.MapControllers();
+
+app.UseSpaImproved(spa =>
+{
+    spa.Options.SourcePath = "ClientApp";
+    // For angular 17
+    spa.Options.CliRegexes = [new Regex(@"Local\:\s+(?<openbrowser>https?\:\/\/(.+))")];
+    if (app.Environment.IsDevelopment())
+    {
+        spa.UseAngularCliServer(npmScript: "start");
+    }
+});
 
 app.Run();
