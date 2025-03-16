3af922b83cc12d0c8c14d849984138098398b41c
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-03-16 10:31:05 +0100

    Top-level statements

--- angular-ssr/Program.cs
+++ angular-ssr/Program.cs
@@ -1,40 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
 
-namespace angular_ssr
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-            builder.Services.AddHealthChecks();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.UseHealthChecks("/healthz");
-            app.MapGet("/", (context) =>
-            {
-                context.Response.Redirect("/WeatherForecast");
-                return Task.CompletedTask;
-            });
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+    app.MapOpenApi();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseHealthChecks("/healthz");
+app.MapGet("/", (context) =>
+{
+    context.Response.Redirect("/WeatherForecast");
+    return Task.CompletedTask;
+});
+app.MapControllers();
+
+app.Run();
