f15177ecf8ada96b07098d8271e1ee16f8c18ad0
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-03-16 09:55:38 +0100

    Add project files.

---
+++ angular-ssr/Program.cs
@@ -0,0 +1,38 @@
+
+namespace angular_ssr
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapGet("/", (context) =>
+            {
+                context.Response.Redirect("/WeatherForecast");
+                return Task.CompletedTask;
+            });
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
